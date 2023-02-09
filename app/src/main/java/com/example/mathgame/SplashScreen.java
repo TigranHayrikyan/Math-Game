b74c28746dc11a754705523f6c1879709fd555ae
Tigran <tigran.hayrikyan@gmail.com>
2023-02-09 13:57:35 +0400

    App with Splash Screen and Icon

---
+++ app/src/main/java/com/example/mathgame/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.example.mathgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.ImageView;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreen extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        imageView = findViewById(R.id.imageView);
+
+        new CountDownTimer(3000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }.start();
+    }
+}
